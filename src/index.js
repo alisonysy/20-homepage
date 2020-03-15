f9aca6ac5bd6dccb453cfc65b7b14f24263cb672
alisonysy <qwen735@aucklanduni.ac.nz>
2020-03-15 11:37:22 +0800

    first init with leancloud setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {leanCloudId,leanCloudKey} from './data-service/leancloud.config';
+var AV = require('leancloud-storage');
+AV.init({
+  appId: leanCloudId,
+  appKey: leanCloudKey,
+  serverURLs: "http://localhost:3000"
+});
+
+console.log('av is',AV)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
