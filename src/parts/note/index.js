f52dce98e0e7ea17eb2ef5d03cf73a6e898c7a5d
alisonysy <qwen735@aucklanduni.ac.nz>
2020-03-27 22:31:14 +0800

    finish tag input field for notes - to note the workaround

--- src/parts/note/index.js
+++ src/parts/note/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import {Card, Button, Rate} from 'antd';
+import {Card, Button, Rate, Input, Tag, Form, Row} from 'antd';
 import './style.css';
+
 
 // This should be in the note list, which is outside of this .js file
 function AddNote(){
@@ -18,20 +19,75 @@ export default class Note extends React.Component{
       todos:[],
       created:undefined,
       tags:[],
-      urgency:0
+      urgency:0,
+      tagInput:''
     }
     this.handleUrgencyChange = this.handleUrgencyChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onTagClosed = this.onTagClosed.bind(this);
+    this.onTagInpChange = this.onTagInpChange.bind(this);
   }
+
+  
 
   handleUrgencyChange(e){
     console.log('----rate changes',e);
     this.setState({...this.state,urgency:e});
   }
 
+  onTagInpChange(e){
+    e.persist();
+    let v = e.target.value;
+    this.setState({tagInput:v});
+    if(v.indexOf(',')!==-1){
+      let tag = v.slice(0,-1);
+      if(!tag.trim().length || this.state.tags.indexOf(tag) !== -1){
+        this.setState({tagInput:''});
+      }
+      this.setState({tags:[...this.state.tags,tag]},(prev)=> console.log(this.state.tags));
+      this.setState({tagInput:''});
+    }
+  }
+
+  onTagClosed(t){
+    t.persist();
+    let tagToRemove = t.target.parentElement.parentElement.textContent;
+    for(let t=0;t<this.tags.length;t++){
+      if(this.tags[t].name===tagToRemove){
+        let copiedTagsArr = [...this.tags];
+        copiedTagsArr.splice(t,1);
+        this.setState({tags:copiedTagsArr});
+      }
+    }
+  }
+
+  onFormSubmit(){
+
+  }
+
   render(){
+    let {tags} = this.state;
     return (
       <Card hoverable className="note" >
-        <Rate character="!" style={{fontSize:20,fontWeight:800,color:'#629bec'}} onChange={this.handleUrgencyChange} value={this.state.urgency}/>
+        <Form onFinish={this.onFormSubmit} >
+          <Form.Item name="urgency" >
+            <Rate character="!" style={{fontSize:20,fontWeight:800,color:'#629bec'}} />
+            {/* onChange={this.handleUrgencyChange} value={this.state.urgency} */}
+          </Form.Item>
+          {/* <Form.Item name="tags" label="Tags" normalize={this.handleTagInput}>
+            <Input onChange={this.onTagInpChange} placeholder="A tag will be generated once there is ','." allowClear={true}/>
+          </Form.Item> */}
+          <Row>
+            <label for="tags">Tags:</label>
+            <input onChange={this.onTagInpChange} value={this.state.tagInput} id="tags"/>
+          </Row>
+          {tags.length>0? 
+            tags.map((t)=>{
+              let k = 'notes-'+t;
+              return <Tag closable={true} onClose={this.onTagClosed} color='#ea45b8' key={k}>{t}</Tag>;
+            })
+          : null}
+        </Form>
       </Card>
     )
   }
